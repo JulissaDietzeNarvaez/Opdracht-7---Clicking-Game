3442133fe85eab48bc6132d7956f0151605567dc
RoyScholtes <56149688+RoyScholtes@users.noreply.github.com>
2019-12-15 19:18:39 +0100

    Add files via upload

---
+++ game.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <script src="/assets/javascript/script.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+
+
+        <div id="game0">
+            <div class="text">
+                <!-- this div exist only for creating a filled space so the buttons are placed on the bottom -->
+            </div>
+            <div class="optionbtnbox">
+                <button type="button" onclick="next1a()" class="btn-dark btn-outline-warning optionbtn">Redirect buttons</button>
+                <button type="button" onclick="next1b()" class="btn-dark btn-outline-warning optionbtn">Redirect buttons</button>
+                <button type="button" onclick="next1c()" class="btn-dark btn-outline-warning optionbtn">Redirect buttons</button>
+            </div>
+        </div>
+
+        <div id="game1a">
+        <div class="text">
+                    <h1>Julissa this is your place. read xinfo.txt to see what to do.</h1>
+                <!-- this div exist only for creating a filled space so the buttons are placed on the bottom -->
+            </div>
+            <div class="optionbtnbox">
+                
+            </div>
+        </div>
+
+        <div id="game1b">
+        <div class="text">
+                <h1>jelle this is your place. read xinfo.txt to see what to do.</h1>
+                <!-- this div exist only for creating a filled space so the buttons are placed on the bottom -->
+            </div>
+            <div class="optionbtnbox">
+                
+            </div>
+        </div>
+
+        <div id="game1c">
+        <div class="text">
+                <h1>Roy this is your place. read xinfo.txt to see what to do.</h1>
+                <!-- this div exist only for creating a filled space so the buttons are placed on the bottom -->
+            </div>
+            <div class="optionbtnbox">
+                
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
