3187bd4f67ad5e5ba10cab2b07f9f9ce321b4dc1
RoyScholtes <56149688+RoyScholtes@users.noreply.github.com>
2019-12-15 20:23:31 +0100

    Add files via upload

--- game.php
+++ game.php
@@ -6,8 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
     <script src="/assets/javascript/script.js"></script>
     <title>Document</title>
@@ -29,32 +31,32 @@
         </div>
 
         <div id="game1a">
-        <div class="text">
-                    <h1>Julissa this is your place. read xinfo.txt to see what to do.</h1>
+            <div class="text">
+                <h1>Julissa this is your place. read xinfo.txt to see what to do.</h1>
                 <!-- this div exist only for creating a filled space so the buttons are placed on the bottom -->
             </div>
             <div class="optionbtnbox">
-                
+
             </div>
         </div>
 
         <div id="game1b">
-        <div class="text">
+            <div class="text">
                 <h1>jelle this is your place. read xinfo.txt to see what to do.</h1>
                 <!-- this div exist only for creating a filled space so the buttons are placed on the bottom -->
             </div>
             <div class="optionbtnbox">
-                
+
             </div>
         </div>
 
         <div id="game1c">
-        <div class="text">
+            <div class="text">
                 <h1>Roy this is your place. read xinfo.txt to see what to do.</h1>
                 <!-- this div exist only for creating a filled space so the buttons are placed on the bottom -->
             </div>
             <div class="optionbtnbox">
-                
+
             </div>
         </div>
 
