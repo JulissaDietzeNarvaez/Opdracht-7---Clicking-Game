d2bf9c57715ee3a950ffe5147faffc9fa065d253
RoyScholtes <56149688+RoyScholtes@users.noreply.github.com>
2019-12-15 19:23:29 +0100

    Add files via upload

---
+++ assets/javascript/script.js
@@ -0,0 +1,35 @@
+function next1a() {
+    var x = document.getElementById("game0");
+    var y = document.getElementById("game1a");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      y.style.display = "block";
+    } else {
+      x.style.display = "block";
+      y.style.display = "none";
+    }
+  }
+
+  function next1b() {
+    var x = document.getElementById("game0");
+    var y = document.getElementById("game1b");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      y.style.display = "block";
+    } else {
+      x.style.display = "block";
+      y.style.display = "none";
+    }
+  }
+
+  function next1c() {
+    var x = document.getElementById("game0");
+    var y = document.getElementById("game1c");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      y.style.display = "block";
+    } else {
+      x.style.display = "block";
+      y.style.display = "none";
+    }
+  }
